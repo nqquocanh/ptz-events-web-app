f5cd7a41192556385bfdf8dad2655666444f9317
nqquocanh <anhnq3@vmodev.com>
2023-01-09 23:49:07 +0700

    update qr

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -39,28 +39,28 @@
           <div>
             <div class="section-title">WELCOME TO HANOI!</div>
             <div class="quick-intro-content">
-              <div class="quick-intro-text">
-                Taking place at a wonderful resort which is about 1-hour drive
-                from Hanoi centre, the event Zouk It Up with the Masters 2023
-                will gather more than 100 dancers from 30 countries and
-                territories including ....
+              <div class="section-text">
+                Taking place at a wonderful resort about 1-hour drive from Hanoi
+                CBD, Zouk It Up with the Masters 2023 will host more than 100
+                zoukers from 20 countries. The very first time such an event
+                happening in Vietnam and the very first time after Covid, such
+                an event is organized in South East Asia.
               </div>
-              <div class="quick-intro-text">
-                The four-day event, the biggest of its kind in the country, will
-                welcome the appearance of 3 world’s top Brazillian Zouk dancers.
+              <div class="section-text">
+                Spreading from 14th to 17th April, Zouk It Up will have 12 hours
+                of workshops and 24 hours of parties with top-notched DJs in the
+                region.
               </div>
-              <div class="quick-intro-text">
-                Alongside, participants can attend ... hour workshops and
-                ...hour social parties during the event.
+              <div class="section-text">
+                Tan Da is a beautiful resort hidden in the charms of the Ba Vi
+                mountains. In the resort, participants can enjoy such leisure
+                activities as kayaking, swimming, sauna, swan-boating, BBQ,
+                inter alia. The resort provides for a tranquil and peaceful
+                atmosphere.
               </div>
-              <div>
-                Every night, a line-up of DJs from ... and Vietnam will turn the
-                dance halls into amazing parties.
-              </div>
-              <div>
-                Forget the city, forget work and enjoy to the fullest - You
-                surely will never forget the special 4-day and 3-night event
-                with us.
+              <div class="section-text">
+                Wait no more. Save the dates! Book your flights and join us!
+                Let's dance our legs off!
               </div>
             </div>
           </div>
@@ -69,7 +69,7 @@
 
       <div id="schedule-section">
         <hr />
-        <div class="section-title">SCHEDULE</div>
+        <div class="section-title">EVENT SCHEDULE</div>
         <div class="schedule-container">
           <div class="schedule-content">
             <div class="schedule-plan">
@@ -77,8 +77,12 @@
                 <div class="schedule-plan-time-date">14</div>
                 <div class="schedule-plan-time-month">April</div>
               </div>
+              <div class="line-event-schedule" />
               <div class="schedule-plan-activities">
-                <div>8:00 PM - 2:00 AM (+1): Pre-Party - HANOI, VIETNAM</div>
+                <div>
+                  <i class="fa-regular fa-clock"></i> 8:00 PM - 2:00 AM (+1):
+                  Pre-Party - Tan Da resort
+                </div>
               </div>
             </div>
           </div>
@@ -89,9 +93,16 @@
                 <div class="schedule-plan-time-date">15</div>
                 <div class="schedule-plan-time-month">April</div>
               </div>
+              <div class="line-event-schedule" />
               <div class="schedule-plan-activities">
-                <div>10:00 AM - 6:00 PM: Workshop - HANOI, VIETNAM</div>
-                <div>8:00 PM - 2:00 AM (+1): Social Party - HANOI, VIETNAM</div>
+                <div>
+                  <i class="fa-regular fa-clock"></i> 10:00 AM - 6:00 PM:
+                  Workshop - Tan Da resort
+                </div>
+                <div>
+                  <i class="fa-regular fa-clock"></i> 8:00 PM - 2:00 AM (+1):
+                  Social Party - Tan Da resort
+                </div>
               </div>
             </div>
           </div>
@@ -102,9 +113,16 @@
                 <div class="schedule-plan-time-date">16</div>
                 <div class="schedule-plan-time-month">April</div>
               </div>
+              <div class="line-event-schedule" />
               <div class="schedule-plan-activities">
-                <div>10:00 AM - 6:00 PM: Workshop - HANOI, VIETNAM</div>
-                <div>8:00 PM - 2:00 AM (+1): Social Party - HANOI, VIETNAM</div>
+                <div>
+                  <i class="fa-regular fa-clock"></i> 10:00 AM - 6:00 PM:
+                  Workshop - Tan Da resort
+                </div>
+                <div>
+                  <i class="fa-regular fa-clock"></i> 8:00 PM - 2:00 AM (+1):
+                  Social Party - Tan Da resort
+                </div>
               </div>
             </div>
           </div>
@@ -115,8 +133,11 @@
                 <div class="schedule-plan-time-date">17</div>
                 <div class="schedule-plan-time-month">April</div>
               </div>
+              <div class="line-event-schedule" />
               <div class="schedule-plan-activities">
-                <div>12:00 PM: Check-out</div>
+                <div>
+                  <i class="fa-regular fa-clock"></i> 12:00 PM: Check-out
+                </div>
               </div>
             </div>
           </div>
@@ -126,15 +147,15 @@
       <div id="activities-section">
         <div class="section-title">ACTIVITIES SUMMARY</div>
         <div class="activities-content">
-          <div class="activities-text">
+          <div class="section-text">
             - Attend Workshops: Learn useful Tips & Tricks from the Brazilian
             Zouk Masters
           </div>
-          <div>
+          <div class="section-text">
             - Social Dance Parties: Immerge in the music, and dance till your
             legs off
           </div>
-          <div>
+          <div class="section-text">
             - Network with Zouk-Addict: Expand your Network all over the world
             with fun and excitement
           </div>
@@ -145,21 +166,21 @@
         <div class="section-title">VENUE</div>
         <div class="venue-container">
           <div class="venue-content">
-            <div class="venue-content-header">
-              Tan Da Spa Resort - Van Hoa, Ba Vi, Hanoi, Vietnam
+            <div class="venue-sub-title">
+              TAN DA SPA RESORT - VAN HOA, BA VI, HANOI, VIETNAM
             </div>
-            <div class="venue-content-text">
-              The resort is within easy reach by a sixty-minute drive from
-              Hanoi, and only one and a half hours drive away from Noi Bai
-              International Airport.
+            <div class="section-text">
+              The resort is within easy reach by a sixty-minute drive from Hanoi
+              CBD, and only one and a half hour drive from Noi Bai International
+              Airport.
             </div>
-            <div>
-              Two clusters which consist of 85 rooms facing Suoi Bon lake view
-              or green garden and share a traditional Tonkin-styled design,
+            <div class="section-text">
+              The resort has 3 sections consisting of 98 rooms facing Suoi Bon
+              lake or green garden and share a traditional Tonkin-styled design,
               providing a great level of comfort while presenting a modern
               standard of Vietnamese luxury.
             </div>
-            <div>
+            <div class="section-text">
               What the resort has to offer includes a wide selection of
               pampering services. Stimulate the senses with rejuvenating
               treatments for mind and body at the Spa or recharge in the outdoor
@@ -167,66 +188,48 @@
               bathing, soaking in aromatherapy with the stream, kayaking, etc
               are also provided.
             </div>
-            <div>
+            <div class="section-text">
               The resort is also home to 3 restaurants, 2 bars, and 1 cafe'. The
               restaurant serves an extensive selection of Vietnamese cuisine and
               fresh tropical fruits. The bar is a wonderful choice for you to
               savor refreshing beverages and cocktails with pairing light
               snacks.
             </div>
-            <div>https://tandasparesort.com.vn/</div>
-          </div>
-
-          <div class="venue-carousel">
-            <b-carousel
-              id="carousel-1"
-              v-model="slide"
-              :interval="4000"
-              controls
-              indicators
-              background="#ababab"
-              img-width="1024"
-              img-height="480"
-              style="text-shadow: 1px 1px 2px #333"
-              @sliding-start="onSlideStart"
-              @sliding-end="onSlideEnd"
+            <a href="https://tandasparesort.com.vn/"
+              >https://tandasparesort.com.vn/</a
             >
-              <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-              <b-carousel-slide>
-                <template #img>
-                  <img
-                    class="d-block img-fluid w-100"
-                    src="../assets/venue/venue-1.jpg"
-                    alt="image slot"
-                  />
-                </template>
-              </b-carousel-slide>
-              <b-carousel-slide>
-                <template #img>
-                  <img
-                    class="d-block img-fluid w-100"
-                    src="../assets/venue/venue-5.jpg"
-                    alt="image slot"
-                  />
-                </template>
-              </b-carousel-slide>
-              <b-carousel-slide>
-                <template #img>
-                  <img
-                    class="d-block img-fluid w-100"
-                    src="../assets/venue/venue-3.jpg"
-                    alt="image slot"
-                  />
-                </template>
-              </b-carousel-slide>
-            </b-carousel>
           </div>
         </div>
+        <div>
+          <b-row>
+            <b-col
+              ><img class="venue-img" src="../assets/venue/venue-1.jpg"
+            /></b-col>
+            <b-col
+              ><img class="venue-img" src="../assets/venue/venue-2.jpg"
+            /></b-col>
+            <b-col
+              ><img class="venue-img" src="../assets/venue/venue-3.jpg"
+            /></b-col>
+          </b-row>
+          <br />
+          <b-row>
+            <b-col
+              ><img class="venue-img" src="../assets/venue/venue-4.jpg"
+            /></b-col>
+            <b-col
+              ><img class="venue-img" src="../assets/venue/venue-5.jpg"
+            /></b-col>
+            <b-col
+              ><img class="venue-img" src="../assets/venue/venue-6.jpg"
+            /></b-col>
+          </b-row>
+        </div>
       </div>
-      <hr />
+      <hr class="artist-line" />
       <div id="artists-section">
         <div class="section-title">ARTISTS</div>
-        <div>
+        <div class="section-text">
           The triple features experienced professionals for teaching Brazilian
           Zouk around the world, which come from different backgrounds but hold
           the same high level of passion regarding their teaching job. From
@@ -235,16 +238,16 @@
         </div>
         <div class="artist-container">
           <div>
-            <div class="artist-name">1. ALISSON SANDI</div>
             <img class="artist-img" src="../assets/artists/1. Alisson.jpg" />
+            <div class="section-sub-title">ALISSON SANDI</div>
           </div>
           <div>
-            <div class="artist-name">2. BRENDA CARVALHO</div>
             <img class="artist-img" src="../assets/artists/2. Brenda.jpg" />
+            <div class="section-sub-title">BRENDA CARVALHO</div>
           </div>
           <div>
-            <div class="artist-name">3. GUI PRADA</div>
             <img class="artist-img" src="../assets/artists/3. Gui.jpg" />
+            <div class="section-sub-title">GUI PRADA</div>
           </div>
         </div>
       </div>
@@ -255,12 +258,34 @@
           <div class="passes-img">
             <img class="passes-img" src="../assets/passes/passes-1.png" />
           </div>
-          <div class="passes-content">
-            <div class="passes-content-header">Get the Ticket.</div>
-            <div class="passes-content-header">Payment.</div>
-            <div class="passes-content-text">
+          <div class="section-text">
+            <div class="section-sub-title">GET THE TICKET.</div>
+            <div class="section-sub-title">PAYMENT.</div>
+            <div class="passes-text">
               For room booking, please contact the resort at: ... or when you
               arrive. Remember to use the code "PTZ" to get the best deal.
+            </div>
+
+            <div>
+              <b-button variant="success" v-b-modal.modal-xl
+                >BOOK NOW!</b-button
+              >
+              <b-modal
+                id="modal-xl"
+                size="xl"
+                title="PAYMENT ACCOUNT"
+                ok-only
+                hide-header-close
+              >
+                <div class="qr-account-img">
+                  <div class="section-sub-title">Foreign Account</div>
+                  <div class="section-sub-title">Vietnamese Account</div>
+                </div>
+                <!-- <div class="section-text">
+                  Please contact to Phuong Trang Zouk's Fanpage for
+                  confirmation!
+                </div> -->
+              </b-modal>
             </div>
           </div>
         </div>
@@ -268,11 +293,11 @@
       <hr />
       <div id="about-ptz-section">
         <div class="section-title">ABOUT PTZ</div>
-        <div>
+        <div class="section-text">
           PTZ (Phuong & Trang Zouk Vietnam) is a dance club founded in 2019 by
           Phuong & Trang who is dedicated and passionate about Brazilian Zouk.
         </div>
-        <div>
+        <div class="section-text">
           With the desire to inspire diverse audiences and build the Zouk
           Community in Vietnam to become stronger, the two dancers have been
           teaching Zouk in Hanoi and making efforts to bring lots of wonderful
@@ -284,7 +309,9 @@
       </div>
     </body>
 
-    <b-button variant="success" class="book-now">Book now!</b-button>
+    <b-button squared variant="success" class="book-now" v-b-modal.modal-xl
+      >Book now!</b-button
+    >
     <div id="app" v-cloak class="back-to-top">
       <transition name="fade">
         <div
@@ -311,11 +338,11 @@
       </transition>
     </div>
     <footer class="footer-section">
-      <hr />
+      <hr class="footer-line" />
       <div class="section-title">SUBSCRIBE TO OUR NEWSLETTER</div>
       <div class="footer-container">
         <div id="subscribe-section">
-          <div class="section-sub-title-media">
+          <div class="section-sub-title">
             ALL INFORMATION AND UPDATED, DIRECTLY TO YOU INBOX
             <div class="text-media">
               Your email address
@@ -328,7 +355,7 @@
           </div>
         </div>
         <div id="contact-channel-section">
-          <div class="section-sub-title-media">OUR CONTACT CHANNEL</div>
+          <div class="section-sub-title">OUR CONTACT CHANNELS</div>
           <div class="media">
             <div>
               <a
@@ -407,20 +434,17 @@ body {
   margin: 0px !important;
   padding: 0px !important;
 }
-
 .nav-bar {
   position: absolute;
   top: 20px;
   right: 40px;
 }
-
 .nav-bar-item {
   text-decoration: none;
   margin: 0px 20px;
   color: white;
   font-size: 20px;
 }
-
 .main-banner-container {
   position: relative;
   text-align: center;
@@ -430,7 +454,6 @@ body {
   width: 100%;
   height: 650px;
 }
-
 .main-banner-title {
   position: absolute;
   top: 50%;
@@ -440,79 +463,83 @@ body {
   width: 80%;
   font-weight: 700;
 }
-
 .main-banner-subtitle {
   font-size: 70%;
   color: white;
   font-weight: 500;
 }
-
 .body-container {
-  width: 90%;
+  width: 70%;
   margin: auto !important;
 }
-
 .section-title {
   font-size: 30px;
   font-weight: 700;
   color: #6305a6;
   margin-top: 30px;
 }
-
+.section-sub-title {
+  margin-top: 20px;
+  font-size: 20px;
+  font-weight: 700;
+}
+.section-text {
+  margin: 15px 0px;
+  font-size: 18px;
+}
 .quick-intro-section {
   top: 130%;
   left: 30%;
   font-size: 20px;
 }
-
 .quick-intro-content {
   width: 67%;
   margin: auto;
   text-align: justify;
   padding: 30px 0px;
 }
-
 #schedule-section {
   background-color: #ebd8f9;
 }
-
 .schedule-plan {
   display: flex;
   justify-content: space-around;
   text-align: left;
 }
-
 .schedule-plan-time {
-  margin-left: 350px;
+  margin-left: 250px;
   margin-bottom: 20px;
 }
 .schedule-plan-time-date {
   font-size: 50px;
   font-weight: 700;
 }
-
 .schedule-plan-time-month {
   font-weight: 600;
   text-align: center;
 }
-
+.line-event-schedule {
+  border: 1px solid;
+  background-color: #6305a6;
+  height: 100px;
+  margin-left: 20px;
+  margin-top: 10px;
+}
 .schedule-plan-activities {
   margin-top: 30px;
   margin-left: 50px;
   width: 800px;
+  font-size: 20px;
 }
-
 .schedule-plan-line {
   width: 60%;
   margin: auto;
-  border: 2px solid;
+  border: 1px solid;
   background-color: #6305a6;
 }
-
 .activities-content {
   margin: 30px 0px;
 }
-
 .venue-container {
   display: flex;
   justify-content: space-between;
@@ -520,23 +547,23 @@ body {
   margin: 30px 0px;
 }
 .venue-content {
-  width: 500px;
   text-align: justify;
 }
-.venue-carousel {
-  margin-left: 80px;
-  width: 600px;
-  height: 400px;
+.venue-sub-title {
+  font-size: 20px;
+  font-weight: 700;
+}
+.venue-img {
+  width: 100%;
+}
+.artist-line {
+  margin-top: 30px;
 }
 .artist-container {
   display: flex;
   justify-content: space-between;
   margin-top: 20px;
   width: 100% !important;
-}
-
-.artist-name {
-  font-weight: 700;
 }
 .artist-img {
   width: 300px;
@@ -547,12 +574,10 @@ body {
   display: flex;
   justify-content: space-around;
 }
-
 .passes-img {
   width: 300px;
   height: 300px;
 }
-
 .passes-content {
   font-size: 30px;
   width: 600px;
@@ -560,10 +585,18 @@ body {
 .passes-content-header {
   font-weight: 700;
 }
-.passes-content-text {
-  font-size: 20px;
+.passes-text {
+  font-size: 18px;
+  width: 50%;
+  margin: 30px auto;
 }
-
+.qr-account-img {
+  display: flex;
+  justify-content: space-around;
+}
+.footer-line {
+  margin-top: 30px;
+}
 .footer-section {
   background-color: #d7bcea;
 }
@@ -579,12 +612,9 @@ body {
   border: none;
   margin-left: 10px;
 }
-
-.section-sub-title-media {
-  font-weight: 700;
-}
 .text-media {
   font-weight: 500;
+  font-size: 18px;
   margin-top: 20px;
 }
 .media {
