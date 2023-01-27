153344b786c98b998f2b2568da898541685a33ab
nqquocanh <anhnq3@vmodev.com>
2023-01-27 22:03:34 +0700

    (fix):link-preview

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VueCountdown from '@chenfengyuan/vue-countdown'
 import VueMeta from 'vue-meta'
+import VueSocialSharing from 'vue-social-sharing'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueSocialSharing)
 Vue.use(VueMeta)
 Vue.config.productionTip = false
 Vue.component(VueCountdown.name, VueCountdown)
