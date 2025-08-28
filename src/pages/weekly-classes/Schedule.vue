b56d98eae3ad3b65185fa7942498c1ef9ba6ead1
nqquocanh <anhnq3@vmodev.com>
2025-08-28 11:49:02 +0700

    fix: fix event schedule

--- src/pages/weekly-classes/Schedule.vue
+++ src/pages/weekly-classes/Schedule.vue
@@ -3,20 +3,18 @@
     <div class="section-title py-4">WEEKLY CLASSES</div>
     <div class="d-flex" style="gap: 20px">
       <div class="w-100">
-        <div class="w-50 m-auto">
+        <div class="body-container m-auto">
           <div class="text-left text-justify section-text pb-4">
             <div class="pb-4">
               <div class="text-center fs-4 pb-4">Wednesday</div>
               <div>8:00PM: Foundation Class</div>
               <div>Address: 4th floor, 9 Tong Duy Tan street</div>
-              <div>
-                Google Map:
-                <a
-                  href="https://maps.app.goo.gl/fAmV3N8JCPGfe3GJ7"
-                  target="_blank"
-                  >https://maps.app.goo.gl/fAmV3N8JCPGfe3GJ7</a
-                >
-              </div>
+              <a
+                href="https://maps.app.goo.gl/fAmV3N8JCPGfe3GJ7"
+                target="_blank"
+              >
+                Google Map Location
+              </a>
             </div>
             <div class="pb-4">
               <div class="text-center fs-4 pb-4">Saturday</div>
@@ -28,14 +26,12 @@
                 Address: 5A floor, 152 Pho Duc Chinh street / Tầng 5A, 152 Phó
                 Đức Chính
               </div>
-              <div>
-                Google Map:
-                <a
-                  href="https://maps.app.goo.gl/UsrGmZ8vGT8NdLNq8"
-                  target="_blank"
-                  >https://maps.app.goo.gl/UsrGmZ8vGT8NdLNq8</a
-                >
-              </div>
+              <a
+                href="https://maps.app.goo.gl/UsrGmZ8vGT8NdLNq8"
+                target="_blank"
+              >
+                Google Map Location
+              </a>
             </div>
             <div>
               <div>Phone: +84 344431591 / +84 326302870</div>
